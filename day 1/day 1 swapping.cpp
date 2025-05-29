9541b471a105c568308cd84ecc18fd6d35519764
jamna sri <jamnavelayutham@gmail.com>
2025-05-29 21:52:24 +0530

    Add files via upload

---
+++ day 1/day 1 swapping.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int x, y, temp;
+    printf("Enter the value of x and y: ");
+    scanf("%d %d", &x, &y);
+
+    printf("Before swapping: x = %d, y = %d\n", x, y);
+
+    temp = x;
+    x = y;
+    y = temp;
+
+    printf("After swapping: x = %d, y = %d\n", x, y);
+    return 0;
+}
+
